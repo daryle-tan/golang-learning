bd5d28a175e847624c50b0265f86500794f71af3
daryle <daryle.tan@gmail.com>
2024-08-25 17:34:10 -0500

    using slices instead of arrays

--- array/sum.go
+++ array/sum.go
@@ -1,10 +1,13 @@
 package array
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-	sum += numbers[i]
+	// for i := 0; i < 5; i++ {
+	// _ is used to ignore the index
+	for _, number := range numbers {
+		sum += number
+	}
+	 return sum
  }
- return sum
-}
+
 
