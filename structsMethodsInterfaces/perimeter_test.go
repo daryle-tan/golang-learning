5b58173f900dd049a7a28009f5ac36930a4ab751
daryle <daryle.tan@gmail.com>
2024-08-26 00:00:59 -0500

    structs, methods, interfaces, table driven tests

---
+++ structsMethodsInterfaces/perimeter_test.go
@@ -0,0 +1,63 @@
+package structsMethodsInterfaces
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	t.Run("find a perimeter of a rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		got := Perimeter(rectangle)
+		want := 40.0
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+}
+
+func TestArea(t *testing.T) {
+
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	}
+	t.Run("find the area of a rectangle", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		// got := rectangle.Area()
+		// want := 72.0
+
+		checkArea(t, rectangle, 72.0)
+	})
+
+	t.Run("find the area of a circle", func(t *testing.T) {
+		circle := Circle{10}
+		// got := circle.Area()
+		// want := 314.1592653589793
+
+		checkArea(t, circle, 314.1592653589793)
+	})
+	// table driven tests
+	t.Run("find area of other shapes", func(t *testing.T) {
+		areaTests := []struct {
+			name string
+			shape Shape		
+			hasArea float64
+		}{
+			{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+			{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+			{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+		}
+
+		for _, tt := range areaTests {
+			t.Run(tt.name, func(t *testing.T) {
+					got := tt.shape.Area()
+					if got != tt.hasArea {
+					t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+					}
+			})
+			
+		}
+	})
+}
